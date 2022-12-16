b64f426932cad2b719a271b0fecffb720edc1a26
Wolfgang De Salvador <wolfgang.desalvador@microsoft.com>
2022-12-16 11:30:39 +0100

    Fix paths

--- specs/scheduler/cluster-init/scripts/00-configure-scheduler.sh
+++ specs/scheduler/cluster-init/scripts/00-configure-scheduler.sh
@@ -1,8 +1,8 @@
 ### Code from MS TechCommunity https://techcommunity.microsoft.com/t5/azure-compute-blog/automate-beeond-filesystem-on-azure-cyclecloud-slurm-cluster/ba-p/3625544
 
 # Download Prolog/Epilog scripts to /sched
-sudo wget -O /sched/slurm_prolog.sh https://raw.githubusercontent.com/themorey/cyclecloud-scripts/main/slurm-beeond/slurm-prolog-beeond.sh
-sudo wget -O /sched/slurm_epilog.sh https://raw.githubusercontent.com/themorey/cyclecloud-scripts/main/slurm-beeond/slurm-epilog-beeond.sh
+sudo wget -O /sched/slurm_prolog.sh https://raw.githubusercontent.com/wolfgang-desalvador/cyclecloud-slurm-beeond/main/specs/scheduler/cluster-init/files/slurm-prolog-beeond.sh
+sudo wget -O /sched/slurm_epilog.sh https://raw.githubusercontent.com/wolfgang-desalvador/cyclecloud-slurm-beeond/main/specs/scheduler/cluster-init/files/slurm-epilog-beeond.sh
 
 # Make the scripts executable
 sudo chmod +x /sched/slurm_*.sh
